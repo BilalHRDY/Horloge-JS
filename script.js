2e5e71639d70fad22c455c9d0c6ae374281dc1f7
Bilal <hardybilal@hotmail.com>
2020-11-08 00:10:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+var container =  document.getElementById('container');
+
+
+var circles = []; 
+var count=0;
+for (var i = 0; i < 12; i++) {
+	circles[count] = createBigCircle(20);
+	circles[count+1] = createLittleCircle(10);
+	circles[count+2]= createLittleCircle(10);
+	circles[count+3] = createLittleCircle(10);
+	circles[count+4] = createLittleCircle(10);
+
+	container.append(circles[count],circles[count+1],circles[count+2],circles[count+3],circles[count+4]);
+	console.log(circles);
+	count+=5;
+	};
+
+function createBigCircle(size){
+	var circle1 = document.createElement('div');
+	circle1.style['width'] = size + 'px';
+	circle1.style['height'] = size + 'px';
+	circle1.style.borderRadius= '50%';
+	circle1.style.backgroundColor= 'black';
+	circle1.style.position = 'absolute';
+	
+	return circle1;
+	};
+
+function createLittleCircle(size){
+	var wraperDiv = document.createElement('div');
+	wraperDiv.style['width'] = size + 'px';
+	wraperDiv.style['height'] = size + 'px';
+	wraperDiv.style.position = 'absolute';
+
+	var circleInside = document.createElement('div');
+	circleInside.style['width'] = size + 'px';
+	circleInside.style['height'] = size + 'px';
+	circleInside.style.borderRadius= '50%';
+	circleInside.style.backgroundColor= 'black';
+	circleInside.style.left= '25%';
+	circleInside.style.top= '25%';
+
+	circleInside.style.position = 'absolute';
+
+	
+	wraperDiv.appendChild(circleInside);
+	
+	
+	return wraperDiv;
+};
+
+
+
+function positionCircle(element, angle){
+	element.style.left= 50 + (40*Math.cos(angle)) + '%';
+	element.style.top= 50 + (40*Math.sin(angle)) + '%';
+};
+
+for (var i = 0; i < circles.length; i++) {
+
+	let angle = ((Math.PI*2) / circles.length) * i ;
+	positionCircle(circles[i], angle);
+};
