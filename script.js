6cff9a4d1671e7f4611e0d1e182e1c2fd71e0e3c
Bilal <hardybilal@hotmail.com>
2020-11-08 01:46:34 +0100

    ajout des nombres

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var container =  document.getElementById('container');
 
+// Création des cercles du cadran:
 
 var circles = []; 
 var count=0;
@@ -11,20 +12,19 @@ for (var i = 0; i < 12; i++) {
 	circles[count+4] = createLittleCircle(20);
 
 	container.append(circles[count],circles[count+1],circles[count+2],circles[count+3],circles[count+4]);
-	console.log(circles);
 	count+=5;
 	};
 
 function createBigCircle(size){
-	var circle1 = document.createElement('div');
-	circle1.style['width'] = size + 'px';
-	circle1.style['height'] = size + 'px';
-	circle1.style.borderRadius= '50%';
-	circle1.style.backgroundColor= 'black';
-	circle1.style.position = 'absolute';
-	circle1.style['transform'] = 'translate(-50%, -50%)';
+	var bigCircle = document.createElement('div');
+	bigCircle.style['width'] = size + 'px';
+	bigCircle.style['height'] = size + 'px';
+	bigCircle.style.borderRadius= '50%';
+	bigCircle.style.backgroundColor= 'black';
+	bigCircle.style.position = 'absolute';
+	bigCircle.style['transform'] = 'translate(-50%, -50%)';
 	
-	return circle1;
+	return bigCircle;
 	};
 
 function createLittleCircle(size){
@@ -41,26 +41,54 @@ function createLittleCircle(size){
 	circleInside.style.backgroundColor= 'black';
 	circleInside.style.left= '25%';
 	circleInside.style.top= '25%';
-
-
 	circleInside.style.position = 'absolute';
-
 	
 	wraperDiv.appendChild(circleInside);
-	
-	
 	return wraperDiv;
 };
 
-
-
 function positionCircle(element, angle){
-	element.style.left= 50 + (40*Math.cos(angle)) + '%';
-	element.style.top= 50 + (40*Math.sin(angle)) + '%';
+	element.style.left= 50 + (45*Math.cos(angle)) + '%';
+	element.style.top= 50 + (45*Math.sin(angle)) + '%';
 };
 
 for (var i = 0; i < circles.length; i++) {
 
 	let angle = ((Math.PI*2) / circles.length) * i ;
 	positionCircle(circles[i], angle);
+};
+
+// Création des nombres du cadran:
+
+function createNumber(size, num){
+	var number = document.createElement('div');
+	number.style['width'] = size + 'px';
+	number.style['height'] = size + 'px';
+	number.style.position = 'absolute';
+	number.textContent = num;
+	number.style['transform'] = 'translate(-50%, -50%)';
+	number.style['display'] = 'flex';
+	number.style['justify-content'] = 'center';
+	number.style['align-items'] = 'center';
+
+	return number;
+}
+
+var numbers = []; 
+for (var i = 0; i < 12; i++) {
+	numbers[i] = createNumber(50, (i+1));
+	
+	container.appendChild(numbers[i]);
+
+	};
+
+function positionNumber(element, angle){
+	element.style.left= 50 + (30*Math.cos(angle)) + '%';
+	element.style.top= 50 + (30*Math.sin(angle)) + '%';
+};
+
+for (var i = 0; i < numbers.length; i++) {
+
+	let angle = ((Math.PI*2) / numbers.length) * i ;
+	positionNumber(numbers[i], angle);
 };
